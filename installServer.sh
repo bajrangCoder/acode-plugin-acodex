14179e237e53d20efd5c63200472a99f52fb8f29
Raunak Raj <nkray21111983@gmail.com>
2023-02-23 03:11:32 +0530

    version 1.0.0 beta released

--- installServer.sh
+++ installServer.sh
@@ -23,12 +23,19 @@ if ! [ -x "$(command -v node)" ]; then
   pkg install nodejs -y
   echo "nodejs installed"
 fi
+
+if ! [ -x "$(command -v python)" ]; then
+  echo "[*] Installing Python"
+  pkg install python -y
+  echo "python installed"
+fi
+
 # check if directory exists
 if [ -d "~/.acodeX-server" ]; then
 	echo "[*] acodeX-server Installation exists already."
 else
 	echo "[*] Installing acodeX-server..."
-    pkg install -y make python build-essential
+    pkg install -y make build-essential
 	mkdir .acodeX-server
     wget https://github.com/bajrangCoder/acode-plugin-acodex/blob/main/server/acodeXServer.zip
 	unzip acodeXServer.zip -d .acodeX-server
