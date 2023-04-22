23288fb3322d16a82daef5f545e53cf5adf2bbfc
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2023-04-22 07:44:26 +0530

    Fixed installation script bug

--- installServer.sh
+++ installServer.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-# check for packages and install if not found
-if ! [ -x "$(command -v unzip)" ]; then
-  echo "\e[1;36m[*] Installing unzip\e[0m"
-  pkg install unzip -y
-fi
-
-if ! [ -x "$(command -v node)" ]; then
-  echo -e "\e[1;36m[*] Installing Nodejs\e[0m"
-  pkg install nodejs -y
-fi
-
-if ! [ -x "$(command -v python)" ]; then
-  echo -e "\e[1;36m[*] Installing Python\e[0m"
-  pkg install python -y
-fi
-
-if ! [ -x "$(command -v make)" ]; then
-  echo -e "\e[1;36m[*] Installing make\e[0m"
-  pkg install make -y
-fi
-
-if ! [ -x "$(command -v build-essential)" ]; then
-  echo -e "\e[1;36m[*] Installing build-essential\e[0m"
-  pkg install build-essential -y
-fi
+function install_packages {
+    # check for packages and install if not found
+    if ! [ -x "$(command -v unzip)" ]; then
+      echo "\e[1;36m[*] Installing unzip\e[0m"
+      pkg install unzip -y
+    fi
+    
+    if ! [ -x "$(command -v node)" ]; then
+      echo -e "\e[1;36m[*] Installing Nodejs\e[0m"
+      pkg install nodejs -y
+    fi
+    
+    if ! [ -x "$(command -v python)" ]; then
+      echo -e "\e[1;36m[*] Installing Python\e[0m"
+      pkg install python -y
+    fi
+    
+    if ! [ -x "$(command -v make)" ]; then
+      echo -e "\e[1;36m[*] Installing make\e[0m"
+      pkg install make -y
+    fi
+    
+    echo -e "\e[1;36m[*] Installing build-essential\e[0m"
+    pkg install build-essential -y
+}
 
 # check if directory exists
 if [ -d "$HOME/.acodeX-server" ]; then
-	echo -e "\e[31m[!] acodeX-server Installation exists already. \e[0m"
+	echo -e "\e[31m[!] `acodeX-server` Installation already exists. \e[0m"
 	exit 1
 else
+    install_packages
 	echo -e "\e[1;36m[*] Installing acodeX-server... \e[0m"
 	mkdir "$HOME/.acodeX-server"
     curl -L -o acodeXServer.zip https://github.com/bajrangCoder/acode-plugin-acodex/raw/main/server/acodeXServer.zip
