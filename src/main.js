05715bfcfd1184e84dad40217c72ec96c051bf60
Raunak Raj <nkray21111983@gmail.com>
2023-07-01 12:20:50 +0530

    Testing

--- src/main.js
+++ src/main.js
@@ -151,6 +151,10 @@ class AcodeX {
             const $controlBtn = tag("div", {
                 className: "control-btn"
             });
+            this.$formatPyCode = tag("button", {
+                className: "format-pycode-btn",
+            });
+            this.$formatPyCode.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M439.8 200.5c-7.7-30.9-22.3-54.2-53.4-54.2h-40.1v47.4c0 36.8-31.2 67.8-66.8 67.8H172.7c-29.2 0-53.4 25-53.4 54.3v101.8c0 29 25.2 46 53.4 54.3 33.8 9.9 66.3 11.7 106.8 0 26.9-7.8 53.4-23.5 53.4-54.3v-40.7H226.2v-13.6h160.2c31.1 0 42.6-21.7 53.4-54.2 11.2-33.5 10.7-65.7 0-108.6zM286.2 404c11.1 0 20.1 9.1 20.1 20.3 0 11.3-9 20.4-20.1 20.4-11 0-20.1-9.2-20.1-20.4.1-11.3 9.1-20.3 20.1-20.3zM167.8 248.1h106.8c29.7 0 53.4-24.5 53.4-54.3V91.9c0-29-24.4-50.7-53.4-55.6-35.8-5.9-74.7-5.6-106.8.1-45.2 8-53.4 24.7-53.4 55.6v40.7h106.9v13.6h-147c-31.1 0-58.3 18.7-66.8 54.2-9.8 40.7-10.2 66.1 0 108.6 7.6 31.6 25.7 54.2 56.8 54.2H101v-48.8c0-35.3 30.5-66.4 66.8-66.4zm-6.7-142.6c-11.1 0-20.1-9.1-20.1-20.3.1-11.3 9-20.4 20.1-20.4 11 0 20.1 9.2 20.1 20.4s-9 20.3-20.1 20.3z"/></svg>`;
             this.$cdBtn = tag("button", {
                 className: "cd-btn",
             });
@@ -163,7 +167,7 @@ class AcodeX {
                 className: "close-terminal-btn",
             });
             this.$closeTermBtn.innerHTML = `<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em"><path fill="currentColor" d="M24 26.1 13.5 36.6q-.45.45-1.05.45-.6 0-1.05-.45-.45-.45-.45-1.05 0-.6.45-1.05L21.9 24 11.4 13.5q-.45-.45-.45-1.05 0-.6.45-1.05.45-.45 1.05-.45.6 0 1.05.45L24 21.9l10.5-10.5q.45-.45 1.05-.45.6 0 1.05.45.45.45.45 1.05 0 .6-.45 1.05L26.1 24l10.5 10.5q.45.45.45 1.05 0 .6-.45 1.05-.45.45-1.05.45-.6 0-1.05-.45Z"/></svg>`;
-            $controlBtn.append(...[this.$cdBtn, this.$hideTermBtn, this.$closeTermBtn]);
+            $controlBtn.append(...[this.$formatPyCode,this.$cdBtn, this.$hideTermBtn, this.$closeTermBtn]);
             this.$terminalHeader.append(...[this.$terminalTitle, $controlBtn]);
             this.$terminalContent = tag("div", {
                 className: "terminal-content",
@@ -195,6 +199,7 @@ class AcodeX {
             this.$closeTermBtn.addEventListener('click', this.closeTerminal.bind(this));
             this.$hideTermBtn.addEventListener('click', this.minimise.bind(this));
             this.$cdBtn.addEventListener('click', this._cdToActiveDir.bind(this));
+            this.$formatPyCode.addEventListener('click', this._formatPythonCode.bind(this));
             document.getElementById("paste-option").addEventListener('click', this._handlePaste.bind(this));
             
             // add event listener for show terminal button 
@@ -676,6 +681,17 @@ class AcodeX {
             return;
         }
         this._sendData(`cd "${realPath}"`);
+    }
+    
+    async _formatPythonCode() {
+        const { activeFile } = editorManager;
+        
+        const realPath = this._convertPath(activeFile.uri);
+        if(!realPath) {
+            window.toast("unsupported path type.", 3000);
+            return;
+        }
+        this._sendData(`black "${realPath}"`);
     }
 
     async destroy() {
