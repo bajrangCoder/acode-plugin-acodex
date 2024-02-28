dba2d175825dd1c5f76bb61fb5a702d74477f759
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2024-02-28 17:34:33 +0530

    Fixed issue of node-gyp on Android platform

--- installServer.sh
+++ installServer.sh
@@ -2,16 +2,25 @@
 
 function install_packages {
     if ! [ -x "$(command -v node)" ]; then
-      echo -e "\e[1;36m[*] Installing Nodejs\e[0m"
-      pkg install nodejs -y
+        echo -e "\e[1;36m[*] Installing Nodejs\e[0m"
+        pkg install nodejs -y
     fi
     if ! [ -x "$(command -v python)" ]; then
-      echo -e "\e[1;36m[*] Installing Python\e[0m"
-      pkg install python -y
+        echo -e "\e[1;36m[*] Installing Python\e[0m"
+        pkg install python -y
     fi
 }
 
+function setup_gyp_folder {
+    cd $HOME && mkdir -p .gyp && echo "{'variables': {'android_ndk_path': ''}}" > .gyp/include.gypi
+}
+
+if [[ "$OSTYPE" == "linux-android"* ]]; then
+    setup_gyp_folder
+fi
+
 install_packages
+
 echo -e "\e[1;36m[*] Installing acodeX-server... \e[0m"
 npm install -g acodex-server
 echo -e '\e[1;32m`acodeX-server` installed successfully. Run `acodeX-server` to start the server. \e[0m'
