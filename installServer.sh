54acbd44842ca8a3d13aec4c3e742b33aba9e04d
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2025-01-31 11:29:12 +0530

    Update installServer.sh

--- installServer.sh
+++ installServer.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
-
-function install_packages {
-    if ! [ -x "$(command -v node)" ]; then
-        echo -e "\e[1;36m[*] Installing Node.js\e[0m"
-        pkg install nodejs -y
-    fi
-    if ! [ -x "$(command -v python)" ]; then
-        echo -e "\e[1;36m[*] Installing Python\e[0m"
-        pkg install python -y
-    fi
-}
-
-function setup_gyp_folder {
-    cd $HOME && mkdir -p .gyp && echo "{'variables': {'android_ndk_path': ''}}" > .gyp/include.gypi
-}
-
-if [[ "$OSTYPE" == "linux-android"* ]]; then
-    setup_gyp_folder
-fi
-
-install_packages
-
 echo -e "\e[1;36m[*] Installing acodeX-server... \e[0m"
-npm install -g acodex-server
-echo -e '\e[1;32m`acodeX-server` installed successfully. Run `acodeX-server` to start the server. \e[0m'
+curl -L https://raw.githubusercontent.com/bajrangCoder/acodex_server/main/install.sh | bash
+echo -e '\e[1;32m`acodeX-server` installed successfully. Run `axs` to start the server. \e[0m'
 
 # Final GUI prompt after installation completes
 echo -e "\e[1;36mDo you want to install GUI-related packages to run gui apps in acodex? (y/n)\e[0m"
