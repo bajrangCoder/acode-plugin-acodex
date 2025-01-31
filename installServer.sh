5cc8d6c60e5f4a764b36777db0239d9584dfbae6
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2025-01-31 11:37:00 +0530

    Update installServer.sh

--- installServer.sh
+++ installServer.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# Check if --gui flag is provided
+# Default to not installing GUI
 INSTALL_GUI=false
-for arg in "$@"; do
-    if [[ "$arg" == "--gui" ]]; then
-        INSTALL_GUI=true
-    fi
+
+# Check for arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --gui) INSTALL_GUI=true ;;
+    esac
+    shift
 done
 
-echo -e "\e[1;36m[*] Installing axs(acodeX-server)... \e[0m"
+echo -e "\e[1;36m[*] Installing acodeX-server... \e[0m"
 curl -L https://raw.githubusercontent.com/bajrangCoder/acodex_server/main/install.sh | bash
-echo -e '\e[1;32m`axs(acodeX-server)` installed successfully. Run `axs` to start the server. \e[0m'
+echo -e '\e[1;32m`acodeX-server` installed successfully. Run `axs` to start the server. \e[0m'
 
+# Only install GUI if the --gui flag was provided
 if $INSTALL_GUI; then
-    echo -e "\e[1;36m[*] Installing GUI-related packages for axs(acodeX-server)...\e[0m"
+    echo -e "\e[1;36m[*] Installing GUI-related packages for acodeX-server...\e[0m"
     pkg install x11-repo -y
     pkg install tigervnc -y
     curl -L https://raw.githubusercontent.com/bajrangCoder/websockify_rs/main/install.sh | bash
-    echo -e "\e[1;32mGUI packages for axs(acodeX-server) installed successfully. Run vncserver command and setup password to get started\e[0m"
+    echo -e "\e[1;32mGUI packages for acodeX-server installed successfully. Run vncserver command and setup password to get started\e[0m"
 else
-    echo -e "\e[1;33mSkipping GUI installation for axs(acodeX-server). You can install it later using --gui flag.\e[0m"
+    echo -e "\e[1;33mSkipping GUI installation for acodeX-server. Use --gui flag to install later.\e[0m"
 fi
