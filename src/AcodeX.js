00b5a9f9b4d707c38899839a6c6ac78b9107355e
Raunak Raj <nkray21111983@gmail.com>
2025-03-04 01:22:41 +0530

    fix: search icon svg

--- src/AcodeX.js
+++ src/AcodeX.js
@@ -188,7 +188,7 @@ export default class AcodeX {
         className: "action-button search-btn",
         ariaLabel: "Search",
       });
-      this.$searchBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-code"><path d="m9 9-2 2 2 2"/><path d="m13 13 2-2-2-2"/><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>`;
+      this.$searchBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-code"><path d="m13 13.5 2-2.5-2-2.5"/><path d="m21 21-4.3-4.3"/><path d="M9 8.5 7 11l2 2.5"/><circle cx="11" cy="11" r="8"/></svg>`;
       this.$cdBtn = tag("button", {
         className: "action-button folder-icon",
         ariaLabel: "Navigate to Folder",
